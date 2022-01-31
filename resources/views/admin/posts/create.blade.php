f16f43f456c4abf21ab28de8a4cf7a6f6412ee0d
elepot87 <78643839+elepot87@users.noreply.github.com>
2022-01-31 20:57:12 +0100

    query DB in PostController, return view e index

---
+++ resources/views/admin/posts/create.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <h1>Create new post</h1>  
+    </div>
+@endsection
---
+++ resources/views/admin/posts/index.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <h1>Blog Posts</h1>
+        @if ($posts->isEmpty())
+            <p>No posts found yet. 
+                <a href="{{route('admin.posts.create')}}">Create a new one</a>
+            </p>
+        @else
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Title</th>
+                        <th colspan="3">Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($posts as $post)
+                        <tr>
+                            <td>{{$post->id}}</td>
+                            <td>{{$post->title}}</td>
+                            <td>SHOW</td>
+                            <td>EDIT</td>
+                            <td>DELETE</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        @endif
+    </div>
+@endsection
