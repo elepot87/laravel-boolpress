cb986f1c22a6d80b3595d6c17fedab2d5399f218
elepot87 <eleonorapotente@gmail.com>
2022-02-08 16:06:38 +0100

    created and methods in App.vue

--- resources/js/views/App.vue
+++ resources/js/views/App.vue
@@ -1,13 +1,32 @@
 <template>
     <div>
-        <h1>Work in progress</h1>
-        <h2>Sito in costruzione</h2>
+        <div class="container my-4">
+            <div class="row">
+                <h1>Il nostro blog</h1>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
     name: "App",
+    components: {},
+    data() {
+        return {
+            posts: null,
+        };
+    },
+    created() {
+        this.getPosts();
+    },
+    methods: {
+        getPosts() {
+            console.log("axios call here");
+        },
+    },
 };
 </script>
 
