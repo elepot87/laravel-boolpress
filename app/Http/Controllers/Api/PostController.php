b682acbec83648cb545de192943c0c5c21db294e
elepot87 <eleonorapotente@gmail.com>
2022-02-08 15:31:10 +0100

    return response in PostController

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -4,11 +4,15 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Post;
 
 class PostController extends Controller
 {
     // Post Archive
     public function index() {
-        return 'Post Json here';
+
+        $post = Post::all();
+
+        return response()->json($post);
     }
 }
