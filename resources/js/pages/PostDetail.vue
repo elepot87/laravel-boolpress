caf2c394685dae01c4267ae16700c99399fb7753
elepot87 <eleonorapotente@gmail.com>
2022-02-10 19:37:21 +0100

    axios per post detail

--- resources/js/pages/PostDetail.vue
+++ resources/js/pages/PostDetail.vue
@@ -1,12 +1,40 @@
 <template>
     <section class="container">
-        <h1>Post detail</h1>
+        <div v-if="post">
+            <h1>{{ post.title }}</h1>
+            <p>{{ post.content }}</p>
+        </div>
+        <div v-else>Loading post</div>
     </section>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
     name: "Post Detail",
+    data() {
+        return {
+            post: null,
+        };
+    },
+    created() {
+        this.getPostDetail();
+    },
+    methods: {
+        getPostDetail() {
+            // Get post from api
+            axios
+                .get(
+                    `http://127.0.0.1:8000/api/posts/${this.$route.params.slug}`
+                )
+                .then((res) => {
+                    console.log(res.data);
+                    this.post = res.data;
+                })
+                .catch((err) => log.error(err));
+        },
+    },
 };
 </script>
 
