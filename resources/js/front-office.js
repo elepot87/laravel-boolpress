d3521cb964775c209dd09de9d66dd4562244c6ed
elepot87 <eleonorapotente@gmail.com>
2022-02-10 18:16:32 +0100

    import routes in front-office.js

--- resources/js/front-office.js
+++ resources/js/front-office.js
@@ -2,8 +2,12 @@
 import Vue from "vue";
 import App from "./views/App";
 
+// App router (rotte per l'app)
+import router from "./routes";
+
 // Init Vue Instance
 const root = new Vue({
     el: "#root",
+    router, //router: router,
     render: (h) => h(App),
 });
