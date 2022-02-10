b68ff2e1cbc64bb701e95d9402f4ddfde902c00c
elepot87 <eleonorapotente@gmail.com>
2022-02-10 18:10:30 +0100

    definizione prima rotta Home

--- resources/js/components/routes.js
+++ resources/js/components/routes.js
@@ -6,3 +6,15 @@ import VueRouter from "vue-router";
 
 // ATTIVAZIONE ROUTER IN VUE.JS
 Vue.use(VueRouter);
+
+// DEFINIZIONE DELLE ROTTE
+const router = new VueRouter({
+    mode: "history", //serve per evitare di avere in url l'#
+    routes: [
+        {
+            path: "/",
+            name: "home",
+            component: Home,
+        },
+    ],
+});
