d0f8066839bd68cdc4c9c3522ca5bbb2e95a1c5b
elepot87 <eleonorapotente@gmail.com>
2022-02-14 15:30:57 +0100

    public in filesystem, link storage and migration

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -17,41 +17,41 @@ class HomeController extends Controller
 
         $now = Carbon::now();
 
-        dump($now);
-        dump($now -> toDateTimeString());
-        dump($now -> toDateString());
+        // dump($now);
+        // dump($now -> toDateTimeString());
+        // dump($now -> toDateString());
 
 
         // data di ieri
         $yesterday = Carbon::yesterday();
-         dump($yesterday -> toDateTimeString());
-          dump($yesterday ->format('l d/m/Y'));
+        //  dump($yesterday -> toDateTimeString());
+        //   dump($yesterday ->format('l d/m/Y'));
 
         //   data di domani
         $tomorrow = Carbon::tomorrow();
-        dump($tomorrow->format('l d/m/Y'));
+        // dump($tomorrow->format('l d/m/Y'));
 
         // CREA DATA CARBON (oggetto di data di carbon)
         $expire = Carbon::create('2038/01/19');
-        dump($expire);
+        // dump($expire);
 
         // COMPARAZIONE
         $first = Carbon::create('2021/01/10');
         $second = Carbon::create('2020/01/10');
 
-        dump($first->lt($second));
+        // dump($first->lt($second));
 
         // DIFFERENZE TRA DUE GIORNI
         $date = Carbon::create('2022/02/06');
         // giorni in relazione con oggi
-        dump($date->diffInDays());
+        // dump($date->diffInDays());
         // giorni in relazione a data stabilita
-        dump($date->diffInDays('2022/02/15'));
+        // dump($date->diffInDays('2022/02/15'));
 
         // TRADUZIONI
         $dt = Carbon::now();
-        dump($dt->isoFormat('dddd DD/MM/YYYY')); //vedere argomenti di Carbon dentro localization
-        dump($dt->locale());
+        // dump($dt->isoFormat('dddd DD/MM/YYYY')); //vedere argomenti di Carbon dentro localization
+        // dump($dt->locale());
 
         return view('admin.home');
     }
